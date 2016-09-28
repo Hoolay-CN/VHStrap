ad1a3eeef834bc7ecb9a17b8d4ee9e30c2a0720f
Charlie <xyhp915@qq.com>
2016-09-28 16:02:22 +0800

    added icons

---
+++ src/components/iconfonts/README.md
@@ -0,0 +1,1 @@
+### Checkout ./resources/demo.html
---
+++ src/components/iconfonts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <i class="iconfont icon-{{ type }}"></i>
+</template>
+
+<script type="text/babel">
+  export default {
+    props: {
+      type: String
+    }
+  }
+</script>
+
+<style lang="css">
+  @import "./resources/iconfont.css";
+</style>
